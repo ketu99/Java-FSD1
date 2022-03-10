d79a801d691902bb23c4692213dc11e25fe10502
Ketaki Tanpure <80162212+ketu99@users.noreply.github.com>
2022-03-10 23:31:59 +0530

    Delete Phase 3 directory

--- Phase 3/Phase3_PracticeProject/FeedbackApplication.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class FeedbackApplication {
-public static void main(String[] args) {
-SpringApplication.run(FeedbackApplication.class, args);
-}
-}
